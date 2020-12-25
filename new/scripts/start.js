8bb61fb6874ccffdbee6fe1cd5012fb4ad94d3ea
Tobias Rohrer <tobias.rohrer@outlook.com>
2020-12-25 12:00:31 +0100

    more cleanup

--- new/scripts/start.js
+++ new/scripts/start.js
@@ -4,7 +4,6 @@ const path = require('path');
 const browserSyncPath = path.resolve(path.dirname(__filename), '../node_modules/.bin/browser-sync');
 
 concurrently([
-    { command: 'node scripts/sb-watch.js', name: 'SB_WATCH', prefixColor: 'bgBlue.bold' },
     {
         command: `"${browserSyncPath}" --reload-delay 2000 --reload-debounce 2000 dist -w --no-online`,
         name: 'SB_BROWSER_SYNC',
